238ea3243dfaf4565f32d87e15621f423309a18c
eldo88 <leegjones88@gmail.com>
2025-04-01 18:45:10 -0600

    Created Street.cs entity

---
+++ StreetSweepingReminder.Api/Entities/Street.cs
@@ -0,0 +1,9 @@
+namespace StreetSweepingReminder.Api.Entities;
+
+public class Street
+{
+    public int Id { get; set; } //PK
+    public string UserId { get; set; } // FK to User
+    public string StreetName { get; set; } = string.Empty;
+    public int? HouseNumber { get; set; }
+}
