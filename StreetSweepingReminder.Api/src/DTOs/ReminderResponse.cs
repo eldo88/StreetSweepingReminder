c45d2948a24a8fd8c364dcda454762bd85920032
eldo88 <leegjones88@gmail.com>
2025-04-02 07:56:30 -0600

    Updated field to use ID

--- StreetSweepingReminder.Api/src/DTOs/ReminderResponse.cs
+++ StreetSweepingReminder.Api/src/DTOs/ReminderResponse.cs
@@ -2,4 +2,4 @@ using StreetSweepingReminder.Api.Entities;
 
 namespace StreetSweepingReminder.Api.DTOs;
 
-public record ReminderResponse(string Message, DateTime ScheduledDateTimeUtc, string Status, string PhoneNumber, Street Street);
+public record ReminderResponse(string Message, DateTime ScheduledDateTimeUtc, string Status, string PhoneNumber, int StreetId);
