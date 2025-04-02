5abca7d67013e75b5260efad4ef8eb8b421278a9
eldo88 <leegjones88@gmail.com>
2025-04-01 18:45:27 -0600

    Added StreetId FK property

--- StreetSweepingReminder.Api/Entities/Reminder.cs
+++ StreetSweepingReminder.Api/Entities/Reminder.cs
@@ -2,10 +2,11 @@ namespace StreetSweepingReminder.Api.Entities;
 
 public class Reminder
 {
-    public int Id { get; set; }
-    public string UserId { get; set; } = string.Empty;
+    public int Id { get; set; } //PK
+    public string UserId { get; set; } = string.Empty; // FK to User
     public string Message { get; set; } = string.Empty;
     public DateTime ScheduledDateTimeUtc { get; set; }
     public string Status { get; set; } = string.Empty; // make this into static string
     public string PhoneNumber { get; set; } = string.Empty;
+    public int StreetId { get; set; } // FK to Street
 }
