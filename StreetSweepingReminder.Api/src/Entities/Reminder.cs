c2995f72d7271c9731414204c739fe4ecbaa59d0
eldo88 <leegjones88@gmail.com>
2025-04-01 19:23:58 -0600

    Added CreatedAt and ModifiedAt properties

--- StreetSweepingReminder.Api/src/Entities/Reminder.cs
+++ StreetSweepingReminder.Api/src/Entities/Reminder.cs
@@ -9,4 +9,6 @@ public class Reminder
     public string Status { get; set; } = string.Empty; // make this into static string
     public string PhoneNumber { get; set; } = string.Empty;
     public int StreetId { get; set; } // FK to Street
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
 }
