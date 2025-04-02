3c1f38169e0bf1057762cb98746ee93dab74e850
eldo88 <leegjones88@gmail.com>
2025-04-02 07:57:10 -0600

    Created mapping extension methods

---
+++ StreetSweepingReminder.Api/src/Extensions/DtoExtensions.cs
@@ -0,0 +1,47 @@
+using StreetSweepingReminder.Api.DTOs;
+using StreetSweepingReminder.Api.Entities;
+
+namespace StreetSweepingReminder.Api.Extensions;
+
+public static class DtoExtensions
+{
+    /* Reminder Entity and DTO Mappings */
+    public static Reminder ToReminderEntity(this CreateReminderDto source)
+    {
+        return new Reminder()
+        {
+            Id = 0,
+            UserId = string.Empty,
+            Message = source.Message,
+            ScheduledDateTimeUtc = source.ScheduledDateTimeUtc,
+            Status = string.Empty,
+            PhoneNumber = source.PhoneNumber,
+            StreetId = source.StreetId,
+            CreatedAt = DateTime.Now
+        };
+    }
+
+    public static ReminderResponse ToReminderResponseDto(this Reminder source)
+    {
+        return new ReminderResponse(
+            source.Message,
+            source.ScheduledDateTimeUtc,
+            source.Status,
+            source.PhoneNumber,
+            source.StreetId);
+    }
+    
+    /* Street Entity to DTO Mappings */
+
+    public static Street ToStreetEntity(this CreateStreetDto source)
+    {
+        return new Street()
+        {
+            Id = 0,
+            UserId = string.Empty,
+            StreetName = source.StreetName,
+            HouseNumber = source.HouseNumber,
+            CreatedAt = DateTime.Now
+        };
+    }
+}
