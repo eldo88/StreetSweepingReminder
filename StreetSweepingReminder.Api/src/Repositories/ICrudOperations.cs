b8de2fcdc6a169256513fd08f05906613635a9b7
eldo88 <leegjones88@gmail.com>
2025-04-02 12:52:10 -0600

    Created generic crud interface for repo actions

---
+++ StreetSweepingReminder.Api/src/Repositories/ICrudOperations.cs
@@ -0,0 +1,10 @@
+namespace StreetSweepingReminder.Api.Repositories;
+
+public interface ICrudOperations<T>
+{
+    Task<int> CreateAsync(T obj);
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<bool> UpdateAsync(T obj);
+    Task<bool> DeleteAsync(int id);
+}
--- StreetSweepingReminder.Api/src/Repositories/ReminderRepository.cs
+++ StreetSweepingReminder.Api/src/Repositories/ReminderRepository.cs
@@ -13,7 +13,27 @@ public class ReminderRepository : IReminderRepository
                             ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
     }
     
-    public Task<int> CreateReminder(Reminder reminder)
+    public Task<int> CreateAsync(Reminder reminder)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Reminder> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Reminder>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Reminder obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id)
     {
         throw new NotImplementedException();
     }
