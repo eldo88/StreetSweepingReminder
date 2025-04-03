b6088ff2fa934c34db238e5e47d29e0cdf7d83fc
eldo88 <leegjones88@gmail.com>
2025-04-03 14:23:43 -0600

    Implemented creating street logic

--- StreetSweepingReminder.Api/src/Services/StreetService.cs
+++ StreetSweepingReminder.Api/src/Services/StreetService.cs
@@ -1,13 +1,39 @@
 using FluentResults;
 using StreetSweepingReminder.Api.DTOs;
+using StreetSweepingReminder.Api.Errors;
+using StreetSweepingReminder.Api.Extensions;
+using StreetSweepingReminder.Api.Repositories;
 
 namespace StreetSweepingReminder.Api.Services;
 
 public class StreetService : IStreetService
 {
-    public Task<Result<int>> CreateStreetAsync(CreateStreetDto command)
+    private readonly ILogger<StreetService> _logger;
+    private readonly IStreetRepository _streetRepository;
+
+    public StreetService(IStreetRepository streetRepository, ILogger<StreetService> logger)
     {
-        throw new NotImplementedException();
+        _streetRepository = streetRepository;
+        _logger = logger;
+    }
+
+    public async Task<Result<int>> CreateStreetAsync(CreateStreetDto command)
+    {
+        var street = command.ToStreetEntity();
+
+        try
+        {
+            var newId = await _streetRepository.CreateAsync(street);
+            return newId <= 0
+                ? Result.Fail<int>(new ApplicationError("Failed to add street to the database."))
+                : Result.Ok(newId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error creating street.");
+            return Result.Fail<int>(
+                new ApplicationError($"An unexpected error occurred while saving the street, {e.Message}"));
+        }
     }
 
     public Task<Result<StreetResponseDto>> GetStreetByIdAsync(int id)
