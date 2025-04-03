6b20893a6ff68ebbdf661d7024cb92ad57e47aa9
eldo88 <leegjones88@gmail.com>
2025-04-03 16:56:26 -0600

    Implemented GetStreetByIdAsync

--- StreetSweepingReminder.Api/src/Services/StreetService.cs
+++ StreetSweepingReminder.Api/src/Services/StreetService.cs
@@ -1,4 +1,5 @@
 using FluentResults;
+using FluentValidation;
 using StreetSweepingReminder.Api.DTOs;
 using StreetSweepingReminder.Api.Errors;
 using StreetSweepingReminder.Api.Extensions;
@@ -10,11 +11,13 @@ public class StreetService : IStreetService
 {
     private readonly ILogger<StreetService> _logger;
     private readonly IStreetRepository _streetRepository;
+    private readonly IValidator<StreetResponseDto> _streetResponseValidator;
 
-    public StreetService(IStreetRepository streetRepository, ILogger<StreetService> logger)
+    public StreetService(IStreetRepository streetRepository, ILogger<StreetService> logger, IValidator<StreetResponseDto> streetResponseValidator)
     {
         _streetRepository = streetRepository;
         _logger = logger;
+        _streetResponseValidator = streetResponseValidator;
     }
 
     public async Task<Result<int>> CreateStreetAsync(CreateStreetDto command)
@@ -36,8 +39,31 @@ public class StreetService : IStreetService
         }
     }
 
-    public Task<Result<StreetResponseDto>> GetStreetByIdAsync(int id)
+    public async Task<Result<StreetResponseDto>> GetStreetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var street = await _streetRepository.GetByIdAsync(id);
+            if (street is null)
+            {
+                return Result.Fail<StreetResponseDto>(new NotFoundError("No street found."));
+            }
+
+            var dto = street.ToStreetResponseDto();
+            var validationResult = await _streetResponseValidator.ValidateAsync(dto);
+            
+            if (!validationResult.IsValid)
+            {
+                return validationResult.ToFluentResult();
+            }
+
+            return Result.Ok(dto);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error retrieving street.");
+            return Result.Fail<StreetResponseDto>(
+                new ApplicationError($"An unexpected error occurred while retrieving the street: {e.Message}"));
+        }
     }
 }
