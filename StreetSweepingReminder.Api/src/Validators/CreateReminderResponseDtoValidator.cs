ee3a0dee64dad16ed49a67dd40ae21b0cc840ed6
eldo88 <leegjones88@gmail.com>
2025-04-03 15:26:25 -0600

    Consolidated WithMessage calls for clarity

--- StreetSweepingReminder.Api/src/Validators/CreateReminderResponseDtoValidator.cs
+++ StreetSweepingReminder.Api/src/Validators/CreateReminderResponseDtoValidator.cs
@@ -9,33 +9,24 @@ public class CreateReminderResponseDtoValidator : AbstractValidator<ReminderResp
     public CreateReminderResponseDtoValidator()
     {
         RuleFor(x => x.Id)
-            .GreaterThan(0)
-            .WithMessage("Error getting Reminder ID.");
+            .GreaterThan(0).WithMessage("Error getting Reminder ID.");
         
         RuleFor(x => x.Message)
-            .NotEmpty()
-            .WithMessage(ValidationMessages.MessageError)
-            .MaximumLength(200)
-            .WithMessage(ValidationMessages.MessageLengthError);
+            .NotEmpty().WithMessage(ValidationMessages.MessageError)
+            .MaximumLength(200).WithMessage(ValidationMessages.MessageLengthError);
         
         RuleFor(x => x.ScheduledDateTimeUtc)
-            .GreaterThan(DateTime.UtcNow)
-            .WithMessage(ValidationMessages.ScheduledDateTimeInvalidDate);
+            .GreaterThan(DateTime.UtcNow).WithMessage(ValidationMessages.ScheduledDateTimeInvalidDate);
 
         RuleFor(x => x.Status)
-            .NotEmpty()
-            .WithMessage("Error with Reminder status.");
+            .NotEmpty().WithMessage("Error with Reminder status.");
         
         RuleFor(x => x.PhoneNumber)
-            .NotEmpty()
-            .WithMessage(ValidationMessages.PhoneNumberRequired)
-            .Length(10, 15)
-            .WithMessage(ValidationMessages.PhoneNumberInvalidLength)
-            .Matches(@"^\+?[1-9]\d{1,14}$")
-            .WithMessage(ValidationMessages.PhoneNumberInvalidFormat);
+            .NotEmpty().WithMessage(ValidationMessages.PhoneNumberRequired)
+            .Length(10, 15).WithMessage(ValidationMessages.PhoneNumberInvalidLength)
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage(ValidationMessages.PhoneNumberInvalidFormat);
         
         RuleFor(x => x.StreetId)
-            .GreaterThan(0)
-            .WithMessage(ValidationMessages.StreetNumberInvalid);
+            .GreaterThan(0).WithMessage(ValidationMessages.StreetNumberInvalid);
     }
 }
