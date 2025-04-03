dcd34f8ccb283b8b3e2be2dd4819e9a9c82107e9
eldo88 <leegjones88@gmail.com>
2025-04-03 15:17:53 -0600

    Implemented validator and added validation messages

--- StreetSweepingReminder.Api/src/Messages/ValidationMessages.cs
+++ StreetSweepingReminder.Api/src/Messages/ValidationMessages.cs
@@ -9,4 +9,7 @@ public readonly struct ValidationMessages
     public static string PhoneNumberInvalidLength => "Phone number seems to be an invalid length.";
     public static string PhoneNumberInvalidFormat => "Invalid phone number format.";
     public static string StreetNumberInvalid => "Valid StreetId is required.";
+    public static string StreetNameError => "Street name must have a value.";
+    public static string StreetNameTooLongError => "Street name cannot exceed 50 characters.";
+    public static string HouseNumberInvalid => "House number must be greater than 0.";
 }
