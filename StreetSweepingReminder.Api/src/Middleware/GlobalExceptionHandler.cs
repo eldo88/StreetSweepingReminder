ebba2d02c48824d297108e99893f519e7b3fa091
eldo88 <leegjones88@gmail.com>
2025-04-03 17:52:14 -0600

    Implemented GlobalExceptionHandler.cs

---
+++ StreetSweepingReminder.Api/src/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace StreetSweepingReminder.Api.Middleware;
+
+public class GlobalExceptionHandler(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception e)
+    {
+        context.Response.ContentType = "application/json";
+
+        var statusCode = SetStatusCode(e);
+
+        context.Response.StatusCode = (int)statusCode;
+
+        var errorResponse = new
+        {
+            ErrorMessage = e.Message,
+            HttpStatusCode = context.Response.StatusCode,
+            ErrorDetails = e.StackTrace,
+            Timestamp = DateTime.UtcNow
+        };
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+    }
+
+    private static HttpStatusCode SetStatusCode(Exception e)
+    {
+        return e switch
+        {
+            ArgumentNullException => HttpStatusCode.BadRequest,
+            ArgumentException => HttpStatusCode.BadRequest,
+            InvalidOperationException => HttpStatusCode.InternalServerError,
+            UnauthorizedAccessException => HttpStatusCode.Unauthorized,
+            DbUpdateException => HttpStatusCode.InternalServerError,
+            _ => HttpStatusCode.InternalServerError
+        };
+    }
+}
