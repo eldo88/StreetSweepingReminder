938e3fb243318a9bd3cfdab444f399b119ba0042
eldo88 <leegjones88@gmail.com>
2025-04-04 12:47:14 -0600

    Updated Streets table and seeded data

--- StreetSweepingReminder.Api/SqlScripts/Script01_CreateStreetsTable.sql
+++ StreetSweepingReminder.Api/SqlScripts/Script01_CreateStreetsTable.sql
@@ -2,9 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS main.Streets (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    UserId TEXT NOT NULL,
     StreetName TEXT NOT NULL,
-    CreatedAt TEXT NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%fZ', 'now')),
-    ModifiedAt TEXT,
-    FOREIGN KEY (UserId) REFERENCES AspNetUsers (Id)
+    ZipCode TEXT NOT NULL, 
+    CreatedAt TEXT NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%fZ', 'now'))
 )
