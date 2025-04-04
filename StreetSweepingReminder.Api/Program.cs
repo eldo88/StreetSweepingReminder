38fb90ee8b6355529cadc8e022c47dae1dfc2637
eldo88 <leegjones88@gmail.com>
2025-04-03 18:56:09 -0600

    Added auth

--- StreetSweepingReminder.Api/Program.cs
+++ StreetSweepingReminder.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text;
 using DbUp;
 using Microsoft.EntityFrameworkCore;
 using StreetSweepingReminder.Api.DbContext;
@@ -6,6 +7,10 @@ using StreetSweepingReminder.Api.Repositories;
 using StreetSweepingReminder.Api.Services;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using StreetSweepingReminder.Api.Entities;
 using StreetSweepingReminder.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -66,6 +71,46 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+    { //Simple password config, will update in the future
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+
+        options.User.RequireUniqueEmail = true; // May remove
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+var jwtKey = builder.Configuration["Jwt:Key"]; // Get from appsettings.json or secrets
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+if (string.IsNullOrEmpty(jwtKey) || string.IsNullOrEmpty(jwtIssuer) || string.IsNullOrEmpty(jwtAudience))
+{
+    throw new InvalidOperationException("JWT Key, Issuer or Audience is not configured properly.");
+}
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.MapInboundClaims = false;
+        
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)
+            )
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -78,9 +123,10 @@ app.UseMiddleware<GlobalExceptionHandler>();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
