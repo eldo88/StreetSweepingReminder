ce02b6c4638dacf0a66f60cfa2d221a98e470db0
eldo88 <leegjones88@gmail.com>
2025-04-04 12:00:22 -0600

    Added user's ID to register and login methods

--- StreetSweepingReminder.Api/src/DTOs/AuthResponseDto.cs
+++ StreetSweepingReminder.Api/src/DTOs/AuthResponseDto.cs
@@ -1,3 +1,3 @@
 namespace StreetSweepingReminder.Api.DTOs;
 
-public record AuthResponseDto(string Token, string Username, string Email);
+public record AuthResponseDto(string Token, string Username, string Email, string UserId);
