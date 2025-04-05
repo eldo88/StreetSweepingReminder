abd9a97d2b0b46650804f0b3dcd80faeb70b80f4
eldo88 <leegjones88@gmail.com>
2025-04-05 15:54:27 -0600

    Added Hero component

--- street-sweeping-reminder-client-app/src/App.vue
+++ street-sweeping-reminder-client-app/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
   <NavBar/>
+  <Hero title="Lee" subtitle="Don't get another street sweeping ticket!"/>
 </template>
