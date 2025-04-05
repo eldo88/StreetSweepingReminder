a420233b5ef313739033c4a0e51413b35d909aa2
eldo88 <leegjones88@gmail.com>
2025-04-04 20:00:33 -0600

    Removed one status and added a new one

--- StreetSweepingReminder.Api/src/Messages/ReminderStatus.cs
+++ StreetSweepingReminder.Api/src/Messages/ReminderStatus.cs
@@ -2,7 +2,7 @@ namespace StreetSweepingReminder.Api.Messages;
 
 public struct ReminderStatus
 {
-    public static string Pending => "Pending";
     public static string Scheduled => "Scheduled";
     public static string Cancelled => "Cancelled";
+    public static string SchedulingFailed => "Failed";
 }
