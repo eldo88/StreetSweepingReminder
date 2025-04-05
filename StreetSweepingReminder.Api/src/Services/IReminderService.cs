d4ce70f3fb9b6782be8e5a92c5392ed22798e7c2
eldo88 <leegjones88@gmail.com>
2025-04-04 20:00:13 -0600

    Uncommented method

--- StreetSweepingReminder.Api/src/Services/IReminderService.cs
+++ StreetSweepingReminder.Api/src/Services/IReminderService.cs
@@ -8,7 +8,7 @@ public interface IReminderService
     Task<Result<int>> CreateReminderAsync(CreateReminderDto command, string userId);
     Task<Result<ReminderResponseDto>> GetReminderByIdAsync(int id);
     Task<Result<IEnumerable<ReminderResponseDto>>> GetUserRemindersAsync(string userId);
-    //Task<Result> UpdateReminderAsync(UpdateReminderCommand command);
+    Task<Result> UpdateReminderAsync(UpdateReminderDto command);
     //Task<Result> DeleteReminderAsync(int id);
     //Task<Result> MarkReminderAsSentAsync(int reminderId);
 }
