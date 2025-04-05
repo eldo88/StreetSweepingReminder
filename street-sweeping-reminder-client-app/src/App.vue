96b345fa5cf94e56e62f13191f14655f4b5cf198
eldo88 <leegjones88@gmail.com>
2025-04-05 17:33:35 -0600

    Added register component and implemented basic call to register endpoint

--- street-sweeping-reminder-client-app/src/App.vue
+++ street-sweeping-reminder-client-app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
+import Register from "@/components/Register.vue";
 </script>
 
 <template>
   <NavBar/>
   <Hero title="Lee" subtitle="Don't get another street sweeping ticket!"/>
+  <Register/>
 </template>
