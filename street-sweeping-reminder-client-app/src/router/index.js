4af7949c9e466a6be3590c149111cab99a006259
eldo88 <leegjones88@gmail.com>
2025-04-07 14:31:01 -0600

    Added routes

--- street-sweeping-reminder-client-app/src/router/index.js
+++ street-sweeping-reminder-client-app/src/router/index.js
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginOrRegisterView from "@/views/LoginOrRegisterView.vue";
+import MainView from "@/views/MainView.vue";
+import {useAuthStore} from "@/stores/auth.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginOrRegisterView,
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: MainView,
+      meta: { requiresAuth: true },
     },
   ],
 })
 
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const isLoggedIn = authStore.token !== null
+
+  if (/*to.meta.requiresAuth &&*/ !isLoggedIn) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 export default router
