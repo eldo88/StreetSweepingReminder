585e525e46ad14e0529ac739156429f35052c62f
eldo88 <leegjones88@gmail.com>
2025-04-07 14:30:28 -0600

    Created views to use with router

---
+++ street-sweeping-reminder-client-app/src/views/LoginOrRegisterView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LoginOrRegister from "@/components/LoginOrRegister.vue";
+import Hero from "@/components/Hero.vue";
+</script>
+
+<template>
+  <Hero title="Lee" subtitle="don't get a street sweeping ticket again"/>
+  <LoginOrRegister/>
+</template>
+
+<style scoped>
+
+</style>
