9ef76596a2aed8ebf07e4e2c783e8b24a1d731db
eldo88 <leegjones88@gmail.com>
2025-04-07 14:30:47 -0600

    Created Main.vue

---
+++ street-sweeping-reminder-client-app/src/components/Main.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Welcome to the main app</h1>
+    <!-- Add NavBar and other main stuff here -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
