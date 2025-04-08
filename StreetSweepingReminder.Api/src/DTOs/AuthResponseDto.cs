c989824d76982b344e4709e7b3f9de570f7ad12c
eldo88 <leegjones88@gmail.com>
2025-04-07 18:27:00 -0600

    Added properties to AuthResponseDto.cs

--- StreetSweepingReminder.Api/src/DTOs/AuthResponseDto.cs
+++ StreetSweepingReminder.Api/src/DTOs/AuthResponseDto.cs
@@ -1,3 +1,9 @@
 namespace StreetSweepingReminder.Api.DTOs;
 
-public record AuthResponseDto(string Token, string Username, string Email, string UserId);
+public record AuthResponseDto(
+    string Token, 
+    string Username, 
+    string Email, 
+    string UserId, 
+    int TokenExpirationInMinutes,
+    DateTime TokenExpirationTimestamp);
