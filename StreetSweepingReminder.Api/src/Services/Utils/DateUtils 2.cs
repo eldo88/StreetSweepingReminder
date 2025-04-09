01513c8dcdae8ac93365ef965201a614f580b6d6
eldo88 <leegjones88@gmail.com>
2025-04-09 16:06:01 -0600

    Removed comments

--- StreetSweepingReminder.Api/src/Services/Utils/DateUtils 2.cs
+++ StreetSweepingReminder.Api/src/Services/Utils/DateUtils 2.cs
@@ -9,25 +9,19 @@ public static class DateUtils
 
         if (weekOfMonth > 0)
         {
-            // Start at the 1st of the month
             var date = new DateTime(year, month, 1);
-
-            // Find the first occurrence of the specified day
+            
             while (date.DayOfWeek != dayOfWeek)
                 date = date.AddDays(1);
-
-            // Add (weekOfMonth - 1) * 7 days to get to the Nth occurrence
+            
             var result = date.AddDays((weekOfMonth - 1) * 7);
-
-            // Ensure it’s still in the same month
+            
             return result.Month == month ? result : null;
         }
-        else // Handle negative values like -1 for "last Tuesday"
+        else // Handle negative values like -1 for "last Tuesday", maybe not needed
         {
-            // Start at the last day of the month
             var date = new DateTime(year, month, DateTime.DaysInMonth(year, month));
-
-            // Find the last occurrence of the specified day
+            
             while (date.DayOfWeek != dayOfWeek)
                 date = date.AddDays(-1);
 
