63027b2615286cad18f92e195d2031162ca21b3c
eldo88 <leegjones88@gmail.com>
2025-04-09 14:14:52 -0600

    Added new method

--- StreetSweepingReminder.Api/src/Services/IReminderScheduler.cs
+++ StreetSweepingReminder.Api/src/Services/IReminderScheduler.cs
@@ -1,8 +1,10 @@
 using FluentResults;
+using StreetSweepingReminder.Api.DTOs;
 
 namespace StreetSweepingReminder.Api.Services;
 
 public interface IReminderScheduler
 {
     Task<Result> ScheduleNotificationJobAsync(int id, DateTime nextSweepTime);
+    Task<Result> CreateReminderNotificationSchedule(CreateReminderDto command);
 }
