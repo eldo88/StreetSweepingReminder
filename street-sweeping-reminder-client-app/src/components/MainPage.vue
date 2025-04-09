d73d8339c61bf1652fa68fd0980eb5f3ee81f7e1
eldo88 <leegjones88@gmail.com>
2025-04-08 18:05:23 -0600

    Added nav abr and hero components to the MainPage component

--- street-sweeping-reminder-client-app/src/components/MainPage.vue
+++ street-sweeping-reminder-client-app/src/components/MainPage.vue
@@ -1,14 +1,11 @@
 <script setup>
-
+import NavBar from './NavBar.vue'
+import Hero from './Hero.vue'
 </script>
 
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">Welcome to the main app</h1>
-    <!-- Add NavBar and other main stuff here -->
-  </div>
+  <NavBar />
+  <Hero />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
