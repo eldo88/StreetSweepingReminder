f138d89a75ba36f29b461e7cd32352026dc9cf29
eldo88 <leegjones88@gmail.com>
2025-04-09 17:34:17 -0600

    Added fields to Dto and creating mapping extension method

--- StreetSweepingReminder.Api/src/Entities/ReminderSchedule.cs
+++ StreetSweepingReminder.Api/src/Entities/ReminderSchedule.cs
@@ -13,6 +13,6 @@ public class ReminderSchedule
     public string TimeOfDay { get; set; } = string.Empty;
     public string TimeZone { get; set; } = string.Empty;
     public bool IsRecurring { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
 }
