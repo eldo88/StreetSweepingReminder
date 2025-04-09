f4ad0877d2985aae3feefeefcc1782324251b517
eldo88 <leegjones88@gmail.com>
2025-04-09 10:19:05 -0600

    Added header to the axios configuration

--- street-sweeping-reminder-client-app/src/services/api.js
+++ street-sweeping-reminder-client-app/src/services/api.js
@@ -2,11 +2,14 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:5010/api/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('jwt')
+    const token = localStorage.getItem('jwtToken')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
