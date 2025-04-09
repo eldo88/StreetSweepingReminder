e2260d42111ea2518bb05fcc3f5ef1f6b761a814
eldo88 <leegjones88@gmail.com>
2025-04-08 18:30:14 -0600

    Continuing shell component setup

---
+++ street-sweeping-reminder-client-app/src/components/ReminderDetails.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  reminder: Object,
+})
+</script>
+
+<template>
+  <!-- int Id, string Message, DateTime ScheduledDateTimeUtc, string Status, string PhoneNumber, int StreetId -->
+
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">{{ reminder.status }}</div>
+        <h3 class="text-xl font-bold">{{ reminder.title }}</h3>
+      </div>
+
+      <div class="mb-5">
+        {{ reminder.Message }}
+      </div>
+
+      <h3 class="text-green-500 mb-2">{{ reminder.phoneNumber }}></h3>
+
+      <div class="border border-gray-100 mb-5"></div>
+
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <!-- maybe wrap it all in here?? -->
+      </div>
+    </div>
+  </div>
+</template>
