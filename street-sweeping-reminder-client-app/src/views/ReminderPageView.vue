329cf6efdaf6a1c46d65a439f4df1ce2ff90dfd1
eldo88 <leegjones88@gmail.com>
2025-04-09 07:33:16 -0600

    Created Reminder related components

---
+++ street-sweeping-reminder-client-app/src/views/ReminderPageView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import Hero from '@/components/Hero.vue'
+import ReminderPage from '@/components/ReminderPage.vue'
+</script>
+
+<template>
+  <NavBar />
+  <Hero />
+  <ReminderPage />
+</template>
