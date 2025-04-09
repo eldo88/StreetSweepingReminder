06f7672b32484277b0685c1c3c35cd0f899a2d46
eldo88 <leegjones88@gmail.com>
2025-04-09 17:14:44 -0600

    Added WeekOfMonth field to CreateReminderDto.cs and added validation to CreateReminderDtoValidator.cs

--- StreetSweepingReminder.Api/src/Validators/CreateReminderDtoValidator.cs
+++ StreetSweepingReminder.Api/src/Validators/CreateReminderDtoValidator.cs
@@ -9,25 +9,22 @@ public class CreateReminderDtoValidator : AbstractValidator<CreateReminderDto>
     public CreateReminderDtoValidator()
     {
         RuleFor(x => x.Message)
-            .NotEmpty()
-            .WithMessage(ValidationMessages.MessageInvalid)
-            .MaximumLength(200)
-            .WithMessage(ValidationMessages.MessageTooLong);
+            .NotEmpty().WithMessage(ValidationMessages.MessageInvalid)
+            .MaximumLength(200).WithMessage(ValidationMessages.MessageTooLong);
         
         RuleFor(x => x.ScheduledDateTimeUtc)
-            .GreaterThan(DateTime.UtcNow)
-            .WithMessage(ValidationMessages.ScheduledDateTimeInvalid);
+            .GreaterThan(DateTime.UtcNow).WithMessage(ValidationMessages.ScheduledDateTimeInvalid);
         
         RuleFor(x => x.PhoneNumber)
-            .NotEmpty()
-            .WithMessage(ValidationMessages.PhoneNumberRequired)
-            .Length(10, 15)
-            .WithMessage(ValidationMessages.PhoneNumberInvalidLength)
-            .Matches(@"^\+?[1-9]\d{1,14}$")
-            .WithMessage(ValidationMessages.PhoneNumberInvalidFormat);
+            .NotEmpty().WithMessage(ValidationMessages.PhoneNumberRequired)
+            .Length(10, 15).WithMessage(ValidationMessages.PhoneNumberInvalidLength)
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage(ValidationMessages.PhoneNumberInvalidFormat);
         
         RuleFor(x => x.StreetId)
-            .GreaterThan(0)
-            .WithMessage(ValidationMessages.StreetNumberInvalid);
+            .GreaterThan(0).WithMessage(ValidationMessages.StreetNumberInvalid);
+
+        RuleFor(x => x.WeekOfMonth)
+            .GreaterThan(0).WithMessage("Week of month must be greater than 0")
+            .LessThanOrEqualTo(4).WithMessage("Week of month must be less than or equal to 4.");
     }
 }
