db7ac5805a775ec7fdf3d0a0578de3fb079bd6d9
eldo88 <leegjones88@gmail.com>
2025-04-08 19:19:34 -0600

    Setup axios globally in the app

--- street-sweeping-reminder-client-app/src/stores/reminder.js
+++ street-sweeping-reminder-client-app/src/stores/reminder.js
@@ -2,30 +2,31 @@ import { defineStore } from 'pinia'
 import { useAuthStore } from './auth'
 import api from '@/services/api'
 
-const authStore = useAuthStore()
-
 // int Id, string Message, DateTime ScheduledDateTimeUtc, string Status, string PhoneNumber, int StreetId
 
 export const useRemindersStore = defineStore('reminders', {
   state: () => ({
-    userId: authStore.getUserId(),
     reminders: [],
   }),
 
   actions: {
-    async getReminders(userId) {
-      if (userId) {
-        api
-          .get('Reminder')
-          .then((response) => {
-            console.log('Data: ', response.data)
-            if (response) {
-              this.reminders = response
-            }
-          })
-          .catch((error) => {
-            console.error('Error: ', error)
-          })
+    async getReminders() {
+      try {
+        const authStore = useAuthStore()
+        const userId = authStore.getUserId()
+
+        if (!userId) {
+          console.warn('No user ID found.')
+          return
+        }
+
+        const response = await api.get('Reminder/${userId}')
+
+        if (response?.data) {
+          this.reminders = response.data
+        }
+      } catch (error) {
+        console.error('Failed to fetch reminders:', error)
       }
     },
   },
