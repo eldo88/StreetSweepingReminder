145ceb2a438d2e6411f6e71109f2a7e71708cc58
eldo88 <leegjones88@gmail.com>
2025-04-09 20:52:28 -0600

    Changed formatting

--- StreetSweepingReminder.Api/src/Services/Utils/DateUtils.cs
+++ StreetSweepingReminder.Api/src/Services/Utils/DateUtils.cs
@@ -5,14 +5,18 @@ public static class DateUtils
     public static DateTime GetNthWeekdayOfMonth(int year, int month, DayOfWeek dayOfWeek, int weekOfMonth)
     {
         if (weekOfMonth == 0 || month < 1 || month > 12)
-            return default;
+        {
+            return default;   
+        }
 
         if (weekOfMonth > 0)
         {
             var date = new DateTime(year, month, 1);
-            
+
             while (date.DayOfWeek != dayOfWeek)
-                date = date.AddDays(1);
+            {
+                date = date.AddDays(1);   
+            }
             
             var result = date.AddDays((weekOfMonth - 1) * 7);
             
@@ -21,9 +25,11 @@ public static class DateUtils
         else // Handle negative values like -1 for "last Tuesday", maybe not needed
         {
             var date = new DateTime(year, month, DateTime.DaysInMonth(year, month));
-            
+
             while (date.DayOfWeek != dayOfWeek)
-                date = date.AddDays(-1);
+            {
+                date = date.AddDays(-1);   
+            }
 
             return date;
         }
