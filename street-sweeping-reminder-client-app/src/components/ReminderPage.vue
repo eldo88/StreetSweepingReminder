d53c763b7f2be1edf10d91e017474567eb612c56
eldo88 <leegjones88@gmail.com>
2025-04-11 17:45:00 -0600

    Added api calls to components

--- street-sweeping-reminder-client-app/src/components/ReminderPage.vue
+++ street-sweeping-reminder-client-app/src/components/ReminderPage.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import ReminderForm from './ReminderForm.vue'
+import ReminderDetails from './ReminderDetails.vue'
+import { useRemindersStore } from '@/stores/reminder.js'
+import { useAuthStore } from '@/stores/auth.js'
+
+const remindersStore = useRemindersStore()
+var reminders = ref([])
+const authStore = useAuthStore()
+var userId = ''
+
+onMounted(() => {
+  userId = authStore.getUserId
+  reminders.value = remindersStore.getReminders(userId)
+})
 </script>
 
 <template>
   <ReminderForm />
+  <ReminderDetails reminder="reminders" />
 </template>
