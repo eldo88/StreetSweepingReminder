436e71ca28a87a33c9142784a88615811b40a046
eldo88 <leegjones88@gmail.com>
2025-04-11 17:44:40 -0600

    Changed link layout

--- street-sweeping-reminder-client-app/src/components/NavBar.vue
+++ street-sweeping-reminder-client-app/src/components/NavBar.vue
@@ -18,7 +18,7 @@ function handleLogout() {
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
           <!-- Logo -->
-          <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+          <a class="flex flex-shrink-0 items-center mr-4" href="/main">
             <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2"
               >Denver Street Sweeping Reminder</span
@@ -33,18 +33,10 @@ function handleLogout() {
               >
               <a
                 href="/reminders"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >Reminders</a
               >
-              <a
-                href="add-job.html"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                >Add Job</a
-              >
-            </div>
-          </div>
-          <div class="md:ml-auto">
-            <div class="flex space-x-2">
+
               <a
                 href="#"
                 @click.prevent="handleLogout"
