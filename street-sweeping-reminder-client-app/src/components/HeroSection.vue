b39396aa51979e1cd00e9673ff15b86b2a6fdd89
eldo88 <leegjones88@gmail.com>
2025-04-10 20:29:11 -0600

    Update component name

--- street-sweeping-reminder-client-app/src/components/HeroSection.vue
+++ street-sweeping-reminder-client-app/src/components/HeroSection.vue
@@ -1,27 +1,23 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps } from 'vue'
 
 defineProps({
   title: {
     type: String,
-    default: 'Hello'
+    default: 'Hello',
   },
   subtitle: {
     type: String,
-    default: 'test'
-  }
-});
+    default: '',
+  },
+})
 </script>
 
 <template>
   <section class="bg-green-700 py-20 mb-4">
-    <div
-      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-    >
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <div class="text-center">
-        <h1
-          class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-        >
+        <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
           {{ title }}
         </h1>
         <p class="my-4 text-xl text-white">
