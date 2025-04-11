022cba53b9e439de0582180ad1b2c0e0e5eee0ee
eldo88 <leegjones88@gmail.com>
2025-04-10 20:07:31 -0600

    Added vee-validate and zod to login form

--- street-sweeping-reminder-client-app/src/components/LoginPage.vue
+++ street-sweeping-reminder-client-app/src/components/LoginPage.vue
@@ -2,11 +2,21 @@
 import { onMounted, ref } from 'vue'
 import { useAuthStore } from '@/stores/auth.js'
 import router from '@/router/index.js'
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { z } from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
 
 const email = ref('')
 const password = ref('')
 
 const store = useAuthStore()
+
+const schema = toTypedSchema(
+  z.object({
+    email: z.string().email('Email not in valid format'),
+    password: z.string().min(5, 'Password must be 5 characters'),
+  }),
+)
 
 const handleSignIn = () => {
   store
@@ -27,26 +37,29 @@ onMounted(() => {
 
 <template>
   <section class="bg-blue-50 py-10">
-    <div class="container mx-auto max-w-md">
+    <div
+      class="container mx-auto px-4 sm:px-6 lg:px-8 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl"
+    >
       <!-- Form Container -->
       <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-        <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Create Account or Sign In</h2>
+        <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Login</h2>
 
-        <form @submit.prevent="handleSignIn">
+        <Form @submit="handleSignIn" :validation-schema="schema">
           <!-- Email Field -->
           <div class="mb-4">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
               Email Address <span class="text-red-500">*</span>
             </label>
-            <input
+            <Field
               v-model="email"
               type="email"
               id="email"
               name="email"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-green-500 focus:ring-1 focus:ring-green-500"
               placeholder="Enter your email"
               required
+              class="input-field"
             />
+            <ErrorMessage name="email" class="text-red-500 text-xs mt-1" />
           </div>
 
           <!-- Password Field -->
@@ -54,29 +67,35 @@ onMounted(() => {
             <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
               Password <span class="text-red-500">*</span>
             </label>
-            <input
+            <Field
               v-model="password"
               type="password"
               id="password"
               name="password"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline focus:border-green-500 focus:ring-1 focus:ring-green-500"
               placeholder="Enter your password"
               required
+              class="input-field"
             />
+            <ErrorMessage name="password" class="text-red-500 text-xs mt-1" />
           </div>
 
           <!-- Button Group -->
           <div class="flex flex-col gap-3">
             <button
-              type="button"
-              @click="handleSignIn"
-              class="w-full bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+              type="submit"
+              class="w-full bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
             >
-              Login
+              Register
             </button>
           </div>
-        </form>
+        </Form>
       </div>
     </div>
   </section>
 </template>
+
+<style scoped lang="postcss">
+.input-field {
+  @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500;
+}
+</style>
