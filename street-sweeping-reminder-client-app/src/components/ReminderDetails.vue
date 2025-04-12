052d19023cd9b6add0fe2ed3fb404f0af603a1af
eldo88 <leegjones88@gmail.com>
2025-04-12 08:50:25 -0600

    Added correct field names to get api results displaying

--- street-sweeping-reminder-client-app/src/components/ReminderDetails.vue
+++ street-sweeping-reminder-client-app/src/components/ReminderDetails.vue
@@ -25,13 +25,4 @@ defineProps({
       <div class="flex flex-col lg:flex-row justify-between mb-4"></div>
     </div>
   </div>
-
-  <!-- <div class="text-gray-600 my-2">{{ reminder.status }}</div>
-  <h3 class="text-xl font-bold">Reminder #{{ reminder.id }}</h3>
-
-  <div class="mb-5">
-    {{ reminder.message }}
-  </div>
-
-  <h3 class="text-green-500 mb-2">{{ reminder.phoneNumber }}</h3> -->
 </template>
