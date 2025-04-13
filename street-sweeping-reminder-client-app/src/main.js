312c73fb52f665c229636936b775f835be8f9b81
eldo88 <leegjones88@gmail.com>
2025-04-13 10:02:26 -0600

    Added toast notifications

--- street-sweeping-reminder-client-app/src/main.js
+++ street-sweeping-reminder-client-app/src/main.js
@@ -9,7 +9,18 @@ import App from './App.vue'
 import router from './router'
 import apiPlugin from './plugins/apiPlugin'
 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 const app = createApp(App)
+
+app.use(Toast, {
+  position: 'top-center',
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+})
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
