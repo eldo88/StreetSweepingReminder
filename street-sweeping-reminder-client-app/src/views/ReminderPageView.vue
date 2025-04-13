0af37da509f2f15f80752f69c345891f616fe926
eldo88 <leegjones88@gmail.com>
2025-04-13 09:44:32 -0600

    Removed commeneted out imports

--- street-sweeping-reminder-client-app/src/views/ReminderPageView.vue
+++ street-sweeping-reminder-client-app/src/views/ReminderPageView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-// import HeroSection from '@/components/HeroSection.vue'
 import ReminderPage from '@/components/ReminderPage.vue'
 </script>
 
