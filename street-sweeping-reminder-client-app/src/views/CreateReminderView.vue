6d24147f551c355c76bbc8a01a58045fa985ef08
eldo88 <leegjones88@gmail.com>
2025-04-13 09:43:59 -0600

    Removed hero section from two views

--- street-sweeping-reminder-client-app/src/views/CreateReminderView.vue
+++ street-sweeping-reminder-client-app/src/views/CreateReminderView.vue
@@ -1,11 +1,10 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import HeroSection from '@/components/HeroSection.vue'
+// import HeroSection from '@/components/HeroSection.vue'
 import CreateReminder from '@/components/CreateReminder.vue'
 </script>
 
 <template>
   <NavBar />
-  <HeroSection />
   <CreateReminder />
 </template>
--- street-sweeping-reminder-client-app/src/views/ReminderPageView.vue
+++ street-sweeping-reminder-client-app/src/views/ReminderPageView.vue
@@ -1,11 +1,10 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import HeroSection from '@/components/HeroSection.vue'
+// import HeroSection from '@/components/HeroSection.vue'
 import ReminderPage from '@/components/ReminderPage.vue'
 </script>
 
 <template>
   <NavBar />
-  <HeroSection />
   <ReminderPage />
 </template>
