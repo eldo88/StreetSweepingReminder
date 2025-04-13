f43661b310abffa1a4fca1768167886661ec9ad1
eldo88 <leegjones88@gmail.com>
2025-04-13 09:16:07 -0600

    Updated logout button to match outher buttons

--- street-sweeping-reminder-client-app/src/components/NavBar.vue
+++ street-sweeping-reminder-client-app/src/components/NavBar.vue
@@ -40,7 +40,7 @@ function handleLogout() {
               <a
                 href="#"
                 @click.prevent="handleLogout"
-                class="text-white bg-blue-600 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
               >
                 Logout
               </a>
