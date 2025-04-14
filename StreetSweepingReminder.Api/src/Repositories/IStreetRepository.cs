ae4853041592a49c36af15574b63c03ed658b80f
eldo88 <leegjones88@gmail.com>
2025-04-14 17:57:03 -0600

    Removed the GetAll methods for streets, data set is too large for a single query

--- StreetSweepingReminder.Api/src/Repositories/IStreetRepository.cs
+++ StreetSweepingReminder.Api/src/Repositories/IStreetRepository.cs
@@ -7,5 +7,4 @@ public interface IStreetRepository
     Task<int> CreateAsync(Street street);
     Task<Street?> GetByIdAsync(int id);
     Task<IEnumerable<Street>> GetByPartialStreetName(string partialStreetName);
-    Task<IEnumerable<Street>> GetAll();
 }
