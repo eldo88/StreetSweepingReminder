10b8ef7ca84d03d1c81f2cceaf89932b9f7c680f
eldo88 <leegjones88@gmail.com>
2025-04-14 19:44:51 -0600

    Updated column name

--- StreetSweepingReminder.Api/SqlScripts/Script04_CreateReminderScheduleTable.sql
+++ StreetSweepingReminder.Api/SqlScripts/Script04_CreateReminderScheduleTable.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS main.ReminderSchedule (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     ReminderId INTEGER NOT NULL, 
     Message TEXT NOT NULL,
-    ReminderDate TEXT NOT NULL,
+    NextNotificationDate TEXT NOT NULL,
     DayOfWeek INTEGER NOT NULL,           -- 0 = Sunday, 1 = Monday, ..., 6 = Saturday
     WeekOfMonth INTEGER NOT NULL,         -- 1 = First, 2 = Second, ..., -1 = Last
     StartMonth INTEGER NOT NULL,          -- 4 for April
