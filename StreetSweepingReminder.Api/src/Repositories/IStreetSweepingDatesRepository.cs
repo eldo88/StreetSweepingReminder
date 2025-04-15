291dc9aa1dab28d01ed426de212830827a5a5d99
eldo88 <leegjones88@gmail.com>
2025-04-14 19:55:34 -0600

    Created shell repo class and interface

---
+++ StreetSweepingReminder.Api/src/Repositories/IStreetSweepingDatesRepository.cs
@@ -0,0 +1,8 @@
+using StreetSweepingReminder.Api.Entities;
+
+namespace StreetSweepingReminder.Api.Repositories;
+
+public interface IStreetSweepingDatesRepository : ICrudRepository<StreetSweepingDates>
+{
+    
+}
