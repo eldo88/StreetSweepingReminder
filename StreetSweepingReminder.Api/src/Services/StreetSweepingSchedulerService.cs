6d52e8a0a4f8ca82cfc0343cb8882f7665d748fa
eldo88 <leegjones88@gmail.com>
2025-04-15 15:04:01 -0600

    Created shell implementation

--- StreetSweepingReminder.Api/src/Services/StreetSweepingSchedulerService.cs
+++ StreetSweepingReminder.Api/src/Services/StreetSweepingSchedulerService.cs
@@ -1,5 +1,7 @@
 using FluentResults;
 using StreetSweepingReminder.Api.DTOs;
+using StreetSweepingReminder.Api.Entities;
+using StreetSweepingReminder.Api.Repositories;
 
 namespace StreetSweepingReminder.Api.Services;
 
@@ -8,9 +10,40 @@ public interface IStreetSweepingSchedulerService
     Task<Result> CreateStreetSweepingSchedule(CreateReminderDto command, int reminderId);
 }
 
-public class StreetSweepingSchedulerService : IStreetSweepingSchedulerService
+public class StreetSweepingSchedulerService : 
+    SchedulerServiceBase<CreateReminderDto, StreetSweepingDates, IStreetSweepingDatesRepository, int,StreetSweepingSchedulerService>,
+    IStreetSweepingSchedulerService
 {
+    public StreetSweepingSchedulerService(ILogger<StreetSweepingSchedulerService> logger, IStreetSweepingDatesRepository repository) : base(logger, repository)
+    {
+    }
+    
     public Task<Result> CreateStreetSweepingSchedule(CreateReminderDto command, int reminderId)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override bool GetIsRecurring(CreateReminderDto command)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override DateTime GetBaseScheduleDate(CreateReminderDto command)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override object[] GetRecurringParameters(CreateReminderDto command)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override StreetSweepingDates MapToEntity(CreateReminderDto command, int parentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void SetScheduleDateOnEntity(StreetSweepingDates entity, DateTime scheduleDate)
     {
         throw new NotImplementedException();
     }
