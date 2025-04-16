df0b256d380208a64ed71c9a6a96f82f6db1aecd
eldo88 <leegjones88@gmail.com>
2025-04-15 20:48:20 -0600

    Fixed return value of controller

--- StreetSweepingReminder.Api/src/Controllers/StreetController.cs
+++ StreetSweepingReminder.Api/src/Controllers/StreetController.cs
@@ -120,7 +120,7 @@ public class StreetController : ControllerBase
         var result = await _streetService.CreateStreetSweepingSchedule(dto, streetId);
         if (result.IsSuccess)
         {
-            return CreatedAtAction(nameof(GetStreetSweepingSchedule), new { streetId });
+            return CreatedAtAction(nameof(GetStreetSweepingSchedule), new { Id = streetId }, streetId);
         }
         
         if (result.HasError<ValidationError>(out var validationErrors))
