71e5e44ace5a125e5d4e2528fe6e46990a4717b0
eldo88 <leegjones88@gmail.com>
2025-04-19 11:46:41 -0600

    Update validator for new fields

--- StreetSweepingReminder.Api/src/Validators/CreateStreetSweepingScheduleDtoValidator.cs
+++ StreetSweepingReminder.Api/src/Validators/CreateStreetSweepingScheduleDtoValidator.cs
@@ -7,11 +7,15 @@ public class CreateStreetSweepingScheduleDtoValidator : AbstractValidator<Create
 {
     public CreateStreetSweepingScheduleDtoValidator()
     {
-        RuleFor(x => x.StreetSweepingDate)
-            .NotEmpty().WithMessage("Street sweeping date must be provided.");
+        RuleFor(x => x.DayOfWeek)
+            .GreaterThanOrEqualTo(1).WithMessage("{PropertyName} is invalid.")
+            .LessThanOrEqualTo(5).WithMessage("{PropertyName} is invalid.");
 
         RuleFor(x => x.WeekOfMonth)
             .GreaterThan(0).WithMessage("Week of the month must be greater than 0.")
             .LessThan(5).WithMessage("Week of the month must be less than 5.");
+
+        RuleFor(x => x.Year)
+            .GreaterThan(2024).WithMessage("{PropertyName} is invalid.");
     }
 }
