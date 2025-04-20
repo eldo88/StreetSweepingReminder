a930f7acdc04a72149c966a92fa74ac73d59dc72
eldo88 <leegjones88@gmail.com>
2025-04-20 17:52:39 -0600

    added new api base

--- street-sweeping-reminder-client-app/src/services/api.js
+++ street-sweeping-reminder-client-app/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5010/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
+console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL)
+// Add a request interceptor to include the JWT token in the headers
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('jwtToken')
