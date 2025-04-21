472f4510e39864a14a561c208d2503c790fdee51
eldo88 <leegjones88@gmail.com>
2025-04-21 10:26:48 -0600

    Added ef annotations to Streets

--- StreetSweepingReminder.Api/src/Entities/Street.cs
+++ StreetSweepingReminder.Api/src/Entities/Street.cs
@@ -1,9 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StreetSweepingReminder.Api.Entities;
 
+[Table("Streets")]
 public class Street
 {
+    [Key]
     public int Id { get; set; } //PK
+    
+    [Required]
     public string StreetName { get; set; } = string.Empty;
+    
+    [Required]
     public int ZipCode { get; set; }
     public DateTime CreatedAt { get; set; }
+    
+    public virtual ICollection<Reminder> Reminders { get; set; } = new List<Reminder>();
+    
+    public virtual ICollection<StreetSweepingDates> StreetSweepingDates { get; set; } = new List<StreetSweepingDates>();
+    
+    public Street()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
 }
