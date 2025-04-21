0f002b231b823dab4cf355cb34475038049c56a4
eldo88 <leegjones88@gmail.com>
2025-04-21 10:31:59 -0600

    Added ef annotations

--- StreetSweepingReminder.Api/src/Entities/StreetSweepingDates.cs
+++ StreetSweepingReminder.Api/src/Entities/StreetSweepingDates.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StreetSweepingReminder.Api.Entities;
 
+[Table("StreetSweepingDates")]
 public class StreetSweepingDates
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public int StreetId { get; set; } // FK to Streets
     public DateTime StreetSweepingDate { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
+    
+    [ForeignKey("StreetId")]
+    public virtual Street Street { get; set; } = null!;
+
+    public StreetSweepingDates()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
 }
