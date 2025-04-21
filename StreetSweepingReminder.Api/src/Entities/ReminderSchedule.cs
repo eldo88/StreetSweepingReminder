02cd23f3c576bbe479c839f4975674a55a53e836
eldo88 <leegjones88@gmail.com>
2025-04-21 10:30:14 -0600

    Added ef annotations

--- StreetSweepingReminder.Api/src/Entities/ReminderSchedule.cs
+++ StreetSweepingReminder.Api/src/Entities/ReminderSchedule.cs
@@ -1,19 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StreetSweepingReminder.Api.Entities;
 
+[Table("ReminderSchedule")]
 public class ReminderSchedule
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public int ReminderId { get; set; }
+    
+    [Required]
     public string Message { get; set; } = string.Empty;
     public DateTime NextNotificationDate { get; set; }
+    
+    [Required]
     public DayOfWeek DayOfWeek { get; set; }
+    
+    [Required]
     public int WeekOfMonth { get; set; }
+    
+    [Required]
     public int StartMonth { get; set; }
+    
+    [Required]
     public int EndMonth { get; set; }
+    
+    [Required]
     public string TimeOfDay { get; set; } = string.Empty;
+    
+    [Required]
     public string TimeZone { get; set; } = string.Empty;
     public bool IsRecurring { get; set; }
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
+    
+    [ForeignKey("ReminderId")]
+    public virtual Reminder Reminder { get; set; } = null!;
+
+    public ReminderSchedule()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
 }
