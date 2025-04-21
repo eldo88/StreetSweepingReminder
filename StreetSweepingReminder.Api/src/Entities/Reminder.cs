19e2777c2697f1713e1fe72a310f717a39316fe4
eldo88 <leegjones88@gmail.com>
2025-04-21 10:35:24 -0600

    Added spacing to properties

--- StreetSweepingReminder.Api/src/Entities/Reminder.cs
+++ StreetSweepingReminder.Api/src/Entities/Reminder.cs
@@ -8,14 +8,19 @@ public class Reminder
 {
     [Key]
     public int Id { get; set; } //PK
+    
     [Required]
     public string UserId { get; set; } = string.Empty; // FK to User
+    
     [Required]
     public string Title { get; set; } = string.Empty;
+    
     [Required]
     public string Status { get; set; } = string.Empty; // make this into static string
+    
     [Required]
     public string PhoneNumber { get; set; } = string.Empty;
+    
     [Required]
     public int StreetId { get; set; } // FK to Street
     public DateTime CreatedAt { get; set; }
