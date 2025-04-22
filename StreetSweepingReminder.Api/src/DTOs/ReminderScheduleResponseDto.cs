ef1ea6f97c5933e55d03aa98d40a0a4ceb99366b
eldo88 <leegjones88@gmail.com>
2025-04-22 09:21:44 -0600

    Updated property name

--- StreetSweepingReminder.Api/src/DTOs/ReminderScheduleResponseDto.cs
+++ StreetSweepingReminder.Api/src/DTOs/ReminderScheduleResponseDto.cs
@@ -1,3 +1,3 @@
 namespace StreetSweepingReminder.Api.DTOs;
 
-public record ReminderScheduleResponseDto(List<ReminderScheduleDto> ReminderSchedule);
+public record ReminderScheduleResponseDto(List<ReminderScheduleDto> Schedule);
