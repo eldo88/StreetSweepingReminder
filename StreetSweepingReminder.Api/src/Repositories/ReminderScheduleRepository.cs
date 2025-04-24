20bb66a136ef9c0dcefe02c17270c543fd71c53e
eldo88 <leegjones88@gmail.com>
2025-04-23 20:41:09 -0600

    Implemented GetAllAsync

--- StreetSweepingReminder.Api/src/Repositories/ReminderScheduleRepository.cs
+++ StreetSweepingReminder.Api/src/Repositories/ReminderScheduleRepository.cs
@@ -36,9 +36,21 @@ internal class ReminderScheduleRepository : RepositoryBase, IReminderScheduleRep
         return reminderDay;
     }
 
-    public Task<IEnumerable<ReminderSchedule>> GetAllAsync(string userId)
+    public async Task<IEnumerable<ReminderSchedule>> GetAllAsync(string userId)
     {
-        throw new NotImplementedException();
+        const string sql =
+            """
+            SELECT rs.Id, rs.ReminderId, rs.NextNotificationDate, rs.NextNotificationDate, rs.DayOfWeek, rs.WeekOfMonth,
+                   rs.StartMonth, rs.EndMonth, rs.TimeOfDay, rs.TimeZone, rs.IsRecurring, rs.IsActive
+            FROM ReminderSchedule as rs
+            JOIN Reminders as r
+            WHERE rs.ReminderId = r.Id
+            AND r.UserId = @userId
+            """;
+
+        using var connection = CreateConnection();
+        var schedule = await connection.QueryAsync<ReminderSchedule>(sql, new { userId });
+        return schedule;
     }
 
     public Task<bool> UpdateAsync(ReminderSchedule obj)
