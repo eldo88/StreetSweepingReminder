7698ff0274cb557958dab3a9ec3d38ea17ac4f8a
eldo88 <leegjones88@gmail.com>
2025-05-03 15:52:43 -0500

    Changed title

--- street-sweeping-reminder-client-app/src/views/RegisterPageView.vue
+++ street-sweeping-reminder-client-app/src/views/RegisterPageView.vue
@@ -4,7 +4,10 @@ import HeroSection from '@/components/HeroSection.vue'
 </script>
 
 <template>
-  <HeroSection title="Lee" subtitle="don't get a street sweeping ticket again" />
+  <HeroSection
+    title="Tired of having to remember street sweeping days?"
+    subtitle="Register for reminder so you never get a ticket again."
+  />
   <RegisterPage />
 </template>
 
