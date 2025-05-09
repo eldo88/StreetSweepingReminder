2d9e552f2e6d31d2f13748be26f78ef4f54357bd
eldo88 <leegjones88@gmail.com>
2025-05-08 18:10:44 -0600

    Implemented spinner for login

--- street-sweeping-reminder-client-app/src/components/LoginForm.vue
+++ street-sweeping-reminder-client-app/src/components/LoginForm.vue
@@ -18,6 +18,7 @@ const props = defineProps({
   },
 })
 
+const isLoading = ref(false)
 const email = ref('')
 const password = ref('')
 const emit = defineEmits(['success'])
@@ -33,6 +34,7 @@ const schema = toTypedSchema(
 )
 
 const handleSignIn = () => {
+  isLoading.value = true
   store
     .login({ email: email.value, password: password.value })
     .then(async (response) => {
@@ -44,8 +46,12 @@ const handleSignIn = () => {
       }
     })
     .catch((error) => {
+      isLoading.value = false
       console.error('Login failed', error)
       toast.error('Login failed. Please check your credentials.')
+    })
+    .finally(() => {
+      isLoading.value = false
     })
 }
 </script>
@@ -108,7 +114,30 @@ const handleSignIn = () => {
               type="submit"
               class="w-full bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
             >
-              Login
+              <template v-if="isLoading">
+                <svg
+                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Logging in...
+              </template>
+              <template v-else> Login </template>
             </button>
           </div>
         </Form>
