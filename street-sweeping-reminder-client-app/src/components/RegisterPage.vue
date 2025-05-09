13b05526816b1b7a1cded6dfa4e3545d1dfd5d10
eldo88 <leegjones88@gmail.com>
2025-05-08 18:15:25 -0600

    Implemented spinner for registering

--- street-sweeping-reminder-client-app/src/components/RegisterPage.vue
+++ street-sweeping-reminder-client-app/src/components/RegisterPage.vue
@@ -7,6 +7,7 @@ import { z } from 'zod'
 import { toTypedSchema } from '@vee-validate/zod'
 import { useToast } from 'vue-toastification'
 
+const isLoading = ref(false)
 const email = ref('')
 const password = ref('')
 const toast = useToast()
@@ -21,11 +22,14 @@ const schema = toTypedSchema(
 )
 
 const handleRegister = async (values) => {
+  isLoading.value = true
   try {
     await store.register(values)
     console.log('Registered and signed in!')
+    isLoading.value = false
     await router.push('/home')
   } catch (error) {
+    isLoading.value = false
     console.error('Registration failed', error)
     toast.error('Registration failed. Please check your credentials.' + error.message)
   }
@@ -90,7 +94,30 @@ const handleRegister = async (values) => {
               type="submit"
               class="w-full bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
             >
-              Register
+              <template v-if="isLoading">
+                <svg
+                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Logging in...
+              </template>
+              <template v-else> Login </template>
             </button>
           </div>
         </Form>
